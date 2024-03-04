af64c962ea9b3fc8f3ae7d2989c6352b4baffcbf
PHILIP OBIORAH <philip.obiorah@outlook.com>
2024-03-04 10:37:26 +0000

    added step and predict function

--- Week_8/perceptron.py
+++ Week_8/perceptron.py
@@ -6,4 +6,17 @@ class Perceptron:
         #Randomly initialize weights and bias
         self.weights = np.random.rand(num_inputs) #initialze weights
         self.bias = np.random.rand(1) #initialize bias
-        self.learning_rate = learning_rate = learning_rate
+        self.learning_rate = learning_rate = learning_rate # Learning rate for training
+
+
+    def step_function(self, x):
+        #Step function to convert any vlaue to 0 or 1
+        return np.where(x >= 0, 1, 0)
+    
+    
+    def predict(self, inputs):
+        #Method to make predictions using the perceptron
+        #compute the weighted sum of inputs and add bias
+        summation = np.dot(input, self.weights) + self.bias
+        #Apply step function to determin e output (0 or 1)
+        return self.step_function(summation)
